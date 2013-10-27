22f8ba5a1238c2c7763f877de6c82d514af97aec
PRADA Hsiung <bear.prada@gmail.com>
2013-10-27 19:54:13 +0800

    initial import

---
+++ sample_app.py
@@ -0,0 +1,67 @@
+import bottle
+from bottle import route, post, run, request, view
+from instagram import client, subscriptions
+
+bottle.debug(True)
+
+CONFIG = {
+    'client_id': '27e229137db647e7a4af91f1d1ba6105',
+    'client_secret': 'f8900228cca14a72a41fbd2a02d9f0f8',
+    'redirect_uri': 'http://dev.atcc.in:8515/oauth_callback'
+}
+
+unauthenticated_api = client.InstagramAPI(**CONFIG)
+
+def process_tag_update(update):
+    print update
+
+reactor = subscriptions.SubscriptionsReactor()
+reactor.register_callback(subscriptions.SubscriptionType.TAG, process_tag_update)
+
+@route('/test')
+@view('hello_template')
+def test():
+    return dict()
+
+@route('/')
+def home():
+    try:
+        url = unauthenticated_api.get_authorize_url(scope=["likes","comments"])
+        return '<a href="%s">Connect with Instagram</a>' % url
+    except Exception, e:
+        print e
+
+@route('/oauth_callback')
+@view('hello_template')
+def on_callback():
+    code = request.GET.get("code")
+    if not code:
+        return 'Missing code'
+    try:
+        access_token = unauthenticated_api.exchange_code_for_access_token(code)
+        if not access_token:
+            return 'Could not get access token'
+        api = client.InstagramAPI(access_token=access_token[0])        
+        tag = api.tag(tag_name='piccollage')
+        #return template('<h1>Hello {{ count }}</h1>', count=tag.media_count)
+        return dict(count=tag.media_count)
+    except Exception, e:
+        print e
+
+@route('/realtime_callback')
+@post('/realtime_callback')
+def on_realtime_callback():
+    mode = request.GET.get("hub.mode")
+    challenge = request.GET.get("hub.challenge")
+    verify_token = request.GET.get("hub.verify_token")
+    if challenge:
+        return challenge
+    else:
+        x_hub_signature = request.header.get('X-Hub-Signature')
+        raw_response = request.body.read()
+        try:
+            reactor.process(CONFIG['client_secret'], raw_response, x_hub_signature)
+        except subscriptions.SubscriptionVerifyError:
+            print "Signature mismatch"
+
+run(host='dev.atcc.in', port=8515, reloader=True)
---
+++ views/hello_template.tpl
@@ -0,0 +1,25 @@
+<html>
+	<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <title>Odometer — Transition numbers with ease</title>
+        <link rel="icon" href="http://static.hubspot.com/favicon.ico">
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="http://github.hubspot.com/odometer/odometer.js"></script>
+        <link rel="stylesheet" href="https://raw.github.com/HubSpot/odometer/master/themes/odometer-theme-train-station.css" />
+<script>
+  $(function(){
+    var starsOdometer = new Odometer({ el: $('.counter')[0], theme: 'train-station', value: '0' });
+    starsOdometer.render();    
+	starsOdometer.update(100);
+	// TODO update peroid by token
+  });
+</script>
+    </head>
+    <body>
+    	<h1>#piccollage on Instagram</h1>
+		<div class="counter" style="font-size: 200;" />
+    </body>
+</html>
